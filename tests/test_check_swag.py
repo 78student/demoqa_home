4d840cd9df6ca19593937fa65abbde7056c09790
78Student <f.t.g@mail.ru>
2025-08-14 19:15:36 +0300

    дз №7 fixes

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -5,9 +5,19 @@ def test_check_icon(browser):
     demo_qa_page = SwagLabs(browser)
     demo_qa_page.visit()
     time.sleep(3)
-#    demo_qa_page.click_on_the_icon()
-    time.sleep(3)
     assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_icon()
+
+def test_check_username(browser):
+    demo_qa_page = SwagLabs(browser)
+    demo_qa_page.visit()
+    time.sleep(3)
+    assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_username()
+
+def test_check_password(browser):
+    demo_qa_page = SwagLabs(browser)
+    demo_qa_page.visit()
+    time.sleep(3)
+    assert demo_qa_page.equal_url()
     assert demo_qa_page.exist_password()
